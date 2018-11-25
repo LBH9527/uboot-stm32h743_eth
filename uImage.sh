f306880505fa86b5c712f0758012a9cb50786d64
huayuguo <huayuguo-22@163.com>
2018-11-25 22:18:15 +0800

    Update uImage.sh
    dst > dts   dir     boot/dts/

--- uImage.sh
+++ uImage.sh
@@ -4,5 +4,5 @@ cp stm32h7.config .config
 make ARCH=arm CROSS_COMPILE=arm-none-eabi- zImage
 make ARCH=arm CROSS_COMPILE=arm-none-eabi- dtbs
 cp ./arch/arm/boot/zImage .
-cp ./arch/arm/boot/dst/stm32h743i-eval.dtb . 
+cp ./arch/arm/boot/dts/stm32h743i-eval.dtb . 
 mkimage -n 'linux' -A arm -O linux -T kernel -C none -a 0x60008000 -e 0x60008000 -d zImage uImage
